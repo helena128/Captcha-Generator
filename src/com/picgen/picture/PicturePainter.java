5343f392ac0f943ef5c4451171f30611f0539da7
Elena Cheprasova <hellogbel@gmail.com>
2018-01-26 19:30:59 +0300

    PicturePainter.java: working algorithm for getting colorful img

--- src/com/picgen/picture/PicturePainter.java
+++ src/com/picgen/picture/PicturePainter.java
@@ -4,54 +4,43 @@ import java.awt.*;
 import java.awt.image.*;
 import java.util.Random;
 
+/**
+ * Creates background for the image
+ */
 public class PicturePainter {
+
+    /**
+     * Creates BufferedImage object, randomly colored
+     * @param height - height of the BufferedImage object
+     * @param width - width of BufferedImage object
+     * @return created image
+     */
     public BufferedImage getBufferedImage(int height, int width) {
-        BufferedImage img = null;
-        int length = ((width + 5) * height) / 7;
-        byte[] imgArr = new byte[length];
-        DataBuffer dataBuffer = new DataBufferByte(imgArr, length);
-        WritableRaster rast = Raster.createPackedRaster(dataBuffer, width, height, 12, null);
+        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        int p; // pixel
 
-        final int firstColor = 0xff0000;//generateFirstColor();
-        final int secondColor = generateSecondColor(firstColor);
-
-        System.out.println(">> First color: " + firstColor + "\tSecond color: " + secondColor);
-
-        // TODO: get normal colors - 1st with random last with lum
-        ColorModel colorModel = new IndexColorModel(1, 2, new byte[] {
-                (byte) firstColor, (byte) secondColor} ,
-                new byte[]  {(byte) firstColor,  (byte) secondColor },  new byte[]  { (byte) firstColor, (byte) secondColor });
-        img = new BufferedImage(colorModel, rast, false, null);
-        Random ran = new Random();
-        ran.nextBytes(imgArr);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                p = getPixel();
+                img.setRGB(x, y, p);
+            }
+        }
         return img;
+
     }
 
     /**
-     * Generates first of the two colors
+     * Creates random color for pixel
      * @return color code
      */
-    private int generateFirstColor() {
+    private int getPixel() {
         Random rand = new Random();
-        int red = rand.nextInt(0xff);
-        int green = rand.nextInt(0xff);
-        int blue = rand.nextInt(0xff);
-        return ((red << 16) | (green << 8) | blue);
-    }
+        int a = rand.nextInt(255);
+        int r = rand.nextInt(255);
+        int g = rand.nextInt(255);
+        int b = rand.nextInt(255);
 
-    /**
-     * Generates 2nd color based on the 1st one
-     * Uses Relative luminance formula to find the color
-     * @return color code
-     */
-    private int generateSecondColor(int firstColor) {
-        ColorComponents comp = new ColorComponents(firstColor);
-        int red = comp.getRComponent();
-        int green = comp.getGComponent();
-        int blue = comp.getBComponent();
-
-        System.out.println(">> RGB: " + red + "\t" + green + "\t" + blue + " ");
-
-        return ((int)(0.2126 * red + 0.7152 * green + 0.0722 * blue));
+        int p = ((a << 24) | (r << 16) | (g << 8) | b);
+        return p;
     }
 }
